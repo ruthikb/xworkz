dca906eee75f77147c3bd99b3ec982fb875a0a7c
kruthik693 <kruthik693@gmail.com>
2025-03-21 12:04:44 +0530

    java constructor file

---
+++ Stadium.java
@@ -0,0 +1,11 @@
+public class Stadium {
+    int id;
+    String name;
+    String location;
+    int capacity;
+    String sportType;
+
+    Stadium(){
+        
+    }
+}
---
+++ StadiumRunner.java
@@ -0,0 +1,78 @@
+public class StadiumRunner{
+
+  public static void main(String[] args) {
+
+    Stadium stadium1 = new Stadium();
+    stadium1.id = 1101;
+    stadium1.name = "Wankhede Stadium";
+    stadium1.location = "Mumbai";
+    stadium1.capacity = 35000;
+    stadium1.sportType = "Cricket";
+    System.out.println("ID is " + stadium1.id);
+
+
+    Stadium stadium2 = new Stadium();
+    stadium2.id = 1102;
+    stadium2.name = "Eden Gardens";
+    stadium2.location = "Kolkata";
+    stadium2.capacity = 68000;
+    stadium2.sportType = "Cricket";
+    System.out.println("ID is " + stadium2.id);
+
+    Stadium stadium3 = new Stadium();
+    stadium3.id = 1103;
+    stadium3.name = "Salt Lake Stadium";
+    stadium3.location = "Kolkata";
+    stadium3.capacity = 85000;
+    stadium3.sportType = "Football";
+    System.out.println("ID is " + stadium3.id);
+
+    Stadium stadium4 = new Stadium();
+    stadium4.id = 1104;
+    stadium4.name = "M. A. Chidambaram Stadium";
+    stadium4.location = "Chennai";
+    stadium4.capacity = 50000;
+    stadium4.sportType = "Cricket";
+    System.out.println("ID is " + stadium4.id);
+
+    Stadium stadium5 = new Stadium();
+    stadium5.id = 1105;
+    stadium5.name = "Feroz Shah Kotla";
+    stadium5.location = "Delhi";
+    stadium5.capacity = 41820;
+    stadium5.sportType = "Cricket";
+    System.out.println("ID is " + stadium5.id);
+
+    Stadium stadium6 = new Stadium();
+    stadium6.id = 1106;
+    stadium6.name = "Jawaharlal Nehru Stadium";
+    stadium6.location = "Delhi";
+    stadium6.capacity = 60000;
+    stadium6.sportType = "Football & Athletics";
+    System.out.println("ID is " + stadium6.id);
+
+    Stadium stadium7 = new Stadium();
+    stadium7.id = 1107;
+    stadium7.name = "Rajiv Gandhi International Stadium";
+    stadium7.location = "Hyderabad";
+    stadium7.capacity = 55000;
+    stadium7.sportType = "Cricket";
+    System.out.println("ID is " + stadium7.id);
+
+    Stadium stadium8 = new Stadium();
+    stadium8.id = 1108;
+    stadium8.name = "Sardar Patel Stadium";
+    stadium8.location = "Ahmedabad";
+    stadium8.capacity = 132000;
+    stadium8.sportType = "Cricket";
+    System.out.println("ID is " + stadium8.id);
+
+    Stadium stadium9 = new Stadium();
+    stadium9.id = 1109;
+    stadium9.name = "Kalinga Stadium";
+    stadium9.location = "Bhubaneswar";
+    stadium9.capacity = 15000;
+    stadium9.sportType = "Hockey & Football";
+    System.out.println("ID is " + stadium9.id);
+  }
+}
