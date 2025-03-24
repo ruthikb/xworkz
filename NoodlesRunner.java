a911739429310946a9f91b05a7f7d220ac77aac1
Kruthik <151733019+ruthikb@users.noreply.github.com>
2025-03-24 11:20:42 +0530

    NoodlesRunner.java

--- NoodlesRunner.java
+++ NoodlesRunner.java
@@ -56,5 +56,40 @@ class{
 		System.out.println("the brand of the noodeles is :"+noodel6.brand);
 		System.out.println("the noOfPacket of the noodeles is:"+noodel6.noOfPacket);
 		System.out.println("the quantity of the noodeles is:"+noodel6.quantity);
+
+		Noodles noodles1 = new Noodles("Maggi" , "Masala" , 12.0);
+			System.out.println("Noodles Brand: "+noodles1.brand);
+			System.out.println("Noodles Flavor: "+noodles1.flavor);
+			System.out.println("Noodles Price: Rs."+noodles1.price);
+
+        Noodles noodles2 = new Noodles("Yippee", "Classic", 15.0);
+			System.out.println("Noodles Brand: "+noodles2.brand);
+			System.out.println("Noodles Flavor: "+noodles2.flavor);
+			System.out.println("Noodles Price: Rs."+noodles2.price);
+			
+        Noodles noodles3 = new Noodles("Top Ramen", "Spicy", 18.0);
+			System.out.println("Noodles Brand: "+noodles3.brand);
+			System.out.println("Noodles Flavor: "+noodles3.flavor);
+			System.out.println("Noodles Price: Rs."+noodles3.price);
+			
+        Noodles noodles4 = new Noodles("Knorr", "Cheese", 20.0);
+			System.out.println("Noodles Brand: "+noodles4.brand);
+			System.out.println("Noodles Flavor: "+noodles4.flavor);
+			System.out.println("Noodles Price: Rs."+noodles4.price);
+			
+        Noodles noodles5 = new Noodles("Ching's", "Schezwan", 25.0);
+			System.out.println("Noodles Brand: "+noodles5.brand);
+			System.out.println("Noodles Flavor: "+noodles5.flavor);
+			System.out.println("Noodles Price: Rs."+noodles5.price);
+			
+        Noodles noodles6 = new Noodles("Patanjali", "Atta Noodles", 10.0);
+			System.out.println("Noodles Brand: "+noodles6.brand);
+			System.out.println("Noodles Flavor: "+noodles6.flavor);
+			System.out.println("Noodles Price: Rs."+noodles6.price);
+			
+        Noodles noodles7 = new Noodles("Sunfeast", "Tomato Twist", 16.0);
+			System.out.println("Noodles Brand: "+noodles7.brand);
+			System.out.println("Noodles Flavor: "+noodles7.flavor);
+			System.out.println("Noodles Price: Rs."+noodles7.price);
 	}
 }
